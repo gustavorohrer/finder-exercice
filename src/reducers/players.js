40b49f5cd84280b80b8d6c5d09de18b6ea8ab9d7
Gustavo Rohrer <gustavo.rohrer@intive.com>
2019-07-05 14:57:22 -0300

    fix players reducer

--- src/reducers/players.js
+++ src/reducers/players.js
@@ -2,8 +2,6 @@ export default function players(state = [], action) {
   switch (action.type) {
     case "INIT":
       return action.payload;
-    case "FILTER":
-      return action.payload;
     default:
       return state;
   }
